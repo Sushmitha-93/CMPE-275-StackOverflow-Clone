c644268488a74fae7c96c0842c2a4e3b9d486d65
Sushmitha D T <dtsushmitha@gmail.com>
2023-09-09 16:04:48 -0700

    Delete kafka directory

--- kafka/validations/Validation.js
+++
@@ -1,8 +0,0 @@
-const { body } = require('express-validator');
-
-exports.registrationValidation = () => {
-    return [
-        body('email', 'Please enter a valid email').isEmail(),
-        body('password', 'Please enter a valid password of minimum 5 characters').isLength({ min: 3 })
-    ];
-}
