b7a5831d152ea977b935c188903b222bea610bb2
Sushmitha D T <dtsushmitha@gmail.com>
2023-09-09 16:04:26 -0700

    Delete client directory

--- client/src/components/util/DefaultImage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import emptyimage from  '../images/emptyimage.png'
-import save from '../util/Util.js'
-
-export default async function emptyimageurl(){
-    return await save("emptyimage" ,emptyimage);
-}
